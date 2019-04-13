852f5802ca2cf4b9e4cffe0da410a7bde63d7233
Faris Widyantho <radenfaris@gmail.com>
2019-04-13 12:20:04 +0700

    banner promotion card

---
+++ lib/constant/banner.dart
@@ -0,0 +1,86 @@
+List bannerPromotion = [{
+    'url': 'http://jkt48.com/sousenkyo2018top3?lang=id',
+    'image': 'https://jkt48.com/images/banner.home.sst3.jpg',
+    'tinyImage': 'http://oi64.tinypic.com/30upoir.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/reqhour2019?lang=id',
+    'image': 'https://jkt48.com/assets/reqhour2019/image/RH19bigbanner.jpg',
+    'tinyImage': 'http://oi67.tinypic.com/jt46fq.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/reqhour2019concert?lang=id',
+    'image': 'https://jkt48.com/assets/reqhour2019concert/image/banner-reqhour2019concert.jpg',
+    'tinyImage': 'http://oi67.tinypic.com/2v362qu.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/hightension?lang=id',
+    'image': 'https://jkt48.com/images/high-tension/big_banner.jpg',
+    'tinyImage': 'http://oi63.tinypic.com/aey5q8.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/theater/schedule?lang=id',
+    'image': 'https://jkt48.com/images/big_banner_fsi.png',
+    'tinyImage': 'http://oi63.tinypic.com/2lmb87q.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/theater/schedule?lang=id',
+    'image': 'https://jkt48.com/images/banner.home.pajama.jpg',
+    'tinyImage': 'http://oi64.tinypic.com/op93ds.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/theater/schedule?lang=id',
+    'image': 'https://jkt48.com/images/banner.home.smet.jpg',
+    'tinyImage': 'http://oi63.tinypic.com/ddkxah.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/pemilihan2018/result?lang=id',
+    'image': 'https://jkt48.com/images/banner.home.pemilihan2018.png',
+    'tinyImage': 'http://oi65.tinypic.com/jrb7f5.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/valkyrie48?lang=id',
+    'image': 'https://jkt48.com/images/banner.home.valkyrie48.jpg',
+    'tinyImage': 'http://oi63.tinypic.com/qpqd8n.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/invitation?lang=jp',
+    'image': 'https://jkt48.com/images/banner.invitation and new friend re.png',
+    'tinyImage': 'http://oi67.tinypic.com/xm64gj.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/',
+    'image': 'https://jkt48.com/images/banner.home.reboost.jpg',
+    'tinyImage': 'http://oi64.tinypic.com/zx6tsp.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/jkt48-academy?lang=id',
+    'image': 'https://jkt48.com/images/banner.home.academy.png',
+    'tinyImage': 'http://oi65.tinypic.com/spjdxg.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/theater/schedule?lang=id',
+    'image': 'https://jkt48.com/images/banner.home.saka.jpg',
+    'tinyImage': 'http://oi63.tinypic.com/24nr71i.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/about/handshake?lang=id',
+    'image': 'https://jkt48.com/images/banner.howtohs.jpg',
+    'tinyImage': 'http://oi68.tinypic.com/2h391e8.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/fanclub?lang=id',
+    'image': 'https://jkt48.com/images/banner.home.ofc.jpg',
+    'tinyImage': 'http://oi66.tinypic.com/etcebc.jpg',
+  },
+  {
+    'url': 'http://jkt48.com/indomaretticket',
+    'image': 'https://jkt48.com/images/banner.home.indomaret.jpg',
+    'tinyImage': 'http://oi68.tinypic.com/j5uqnn.jpg',
+  },
+  {
+    'url': 'https://www.tokopedia.com/officialjkt48',
+    'image': 'https://jkt48.com/images/banner.home.tokopedia.jpg',
+    'tinyImage': 'http://oi65.tinypic.com/2isvyfl.jpg',
+  },
+];
--- lib/screen/Index_screen.dart
+++ lib/screen/Index_screen.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'dart:async';
 import 'package:package_info/package_info.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'package:redux/redux.dart';
 import 'package:flutter_redux/flutter_redux.dart';
@@ -15,6 +16,7 @@ import '../action/device_action.dart';
 import '../component/left_drawer.dart';
 
 import '../constant/show.dart';
+import '../constant/banner.dart';
 
 import '../screen/show_more_screen.dart';
 
@@ -31,6 +33,7 @@ class _IndexScreenState extends State<IndexScreen> {
   String version;
   String buildNumber;
   int current = 0;
+  int currentBanner = 0;
 
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
@@ -81,6 +84,235 @@ class _IndexScreenState extends State<IndexScreen> {
     return result;
   }
 
+  List<Widget> promotion() {
+    return [
+      Container(
+        height: 50,
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey)),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              'Promotion:',
+              style: TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+            ),
+            // GestureDetector(
+            //   onTap: () {
+            //     Navigator.push(
+            //       context,
+            //       MaterialPageRoute(
+            //         builder: (context) => ShowMoreScreen(
+            //               data: data,
+            //             ),
+            //       ),
+            //     );
+            //   },
+            //   child: Row(
+            //     children: <Widget>[
+            //       Text(
+            //         'Lihat semua',
+            //         style: TextStyle(fontSize: 14),
+            //       ),
+            //       Icon(Icons.chevron_right)
+            //     ],
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+      CarouselSlider(
+        height: 150,
+        viewportFraction: 0.62,
+        aspectRatio: 2.0,
+        // enlargeCenterPage: true,
+        onPageChanged: (index) {
+          setState(() {
+            currentBanner = index;
+          });
+        },
+        items: bannerPromotion.map((banner) {
+          return Builder(
+            builder: (BuildContext context) {
+              return GestureDetector(
+                onTap: () {
+                  _launchURL(banner['url']);
+                },
+                child: Container(
+                  margin: EdgeInsets.all(5.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    child: Stack(children: <Widget>[
+                      Image.network(banner['tinyImage'],
+                          fit: BoxFit.cover, height: 150.0),
+                    ]),
+                  ),
+                ),
+              );
+            },
+          );
+        }).toList(),
+      ),
+    ];
+  }
+
+  Widget body() {
+    return Container(
+      color: Colors.grey[200],
+      child: ListView(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+              ),
+              Container(
+                height: 50,
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(color: Colors.grey)),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Sedang Tampil:',
+                      style:
+                          TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ShowMoreScreen(
+                                  data: data,
+                                ),
+                          ),
+                        );
+                      },
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            'Lihat semua',
+                            style: TextStyle(fontSize: 14),
+                          ),
+                          Icon(Icons.chevron_right)
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+              ),
+              CarouselSlider(
+                height: 250,
+                viewportFraction: 0.6,
+                // aspectRatio: 2.0,
+                enlargeCenterPage: true,
+                onPageChanged: (index) {
+                  setState(() {
+                    current = index;
+                  });
+                },
+                items: showList.map((show) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ShowMoreScreen(
+                                    data: filterData(show),
+                                  ),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          margin: EdgeInsets.all(5.0),
+                          child: ClipRRect(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5.0)),
+                            child: Stack(children: <Widget>[
+                              Image.network(show['miniBanner'],
+                                  fit: BoxFit.cover, height: 250.0),
+                              Positioned(
+                                bottom: 0.0,
+                                left: 0.0,
+                                right: 0.0,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                      colors: [
+                                        Color.fromARGB(200, 0, 0, 0),
+                                        Color.fromARGB(0, 0, 0, 0)
+                                      ],
+                                      begin: Alignment.bottomCenter,
+                                      end: Alignment.topCenter,
+                                    ),
+                                  ),
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 10.0, horizontal: 20.0),
+                                  child: Text(
+                                    'Beli Tiket',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            ]),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 5),
+                height: 90,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '${getTitle(current)}',
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text('${getTeam(current)}'),
+                  ],
+                ),
+              ),
+              promotion()[0],
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+              ),
+              promotion()[1],
+              Padding(
+                padding: EdgeInsets.only(top: 30),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return StoreConnector(
@@ -114,151 +346,8 @@ class _IndexScreenState extends State<IndexScreen> {
           body: data.length == 0
               ? _loadingView
               : RefreshIndicator(
-                  onRefresh: () {
-                    return fetchData();
-                  },
-                  child: Container(
-                    color: Colors.grey[200],
-                    child: ListView(
-                      children: <Widget>[
-                        Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.only(top: 10),
-                            ),
-                            Container(
-                              height: 50,
-                              padding: EdgeInsets.symmetric(horizontal: 15),
-                              child: Row(
-                                mainAxisSize: MainAxisSize.max,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: <Widget>[
-                                  Text(
-                                    'Sedang Tampil:',
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontStyle: FontStyle.italic),
-                                  ),
-                                  GestureDetector(
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => ShowMoreScreen(
-                                                data: data,
-                                              ),
-                                        ),
-                                      );
-                                    },
-                                    child: Row(
-                                      children: <Widget>[
-                                        Text(
-                                          'Lihat semua',
-                                          style: TextStyle(fontSize: 14),
-                                        ),
-                                        Icon(Icons.chevron_right)
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 10),
-                            ),
-                            CarouselSlider(
-                              height: 350,
-                              viewportFraction: 0.8,
-                              // aspectRatio: 2.0,
-                              enlargeCenterPage: true,
-                              onPageChanged: (index) {
-                                setState(() {
-                                  current = index;
-                                });
-                              },
-                              items: showList.map((show) {
-                                return Builder(
-                                  builder: (BuildContext context) {
-                                    return GestureDetector(
-                                      onTap: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                ShowMoreScreen(
-                                                  data: filterData(show),
-                                                ),
-                                          ),
-                                        );
-                                      },
-                                      child: Container(
-                                        margin: EdgeInsets.all(5.0),
-                                        child: ClipRRect(
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(5.0)),
-                                          child: Stack(children: <Widget>[
-                                            Image.network(show['miniBanner'],
-                                                fit: BoxFit.cover,
-                                                height: 350.0),
-                                            Positioned(
-                                              bottom: 0.0,
-                                              left: 0.0,
-                                              right: 0.0,
-                                              child: Container(
-                                                decoration: BoxDecoration(
-                                                  gradient: LinearGradient(
-                                                    colors: [
-                                                      Color.fromARGB(
-                                                          200, 0, 0, 0),
-                                                      Color.fromARGB(0, 0, 0, 0)
-                                                    ],
-                                                    begin:
-                                                        Alignment.bottomCenter,
-                                                    end: Alignment.topCenter,
-                                                  ),
-                                                ),
-                                                padding: EdgeInsets.symmetric(
-                                                    vertical: 10.0,
-                                                    horizontal: 20.0),
-                                                child: Text(
-                                                  'Beli Tiket',
-                                                  style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontSize: 20.0,
-                                                    fontWeight: FontWeight.bold,
-                                                  ),
-                                                  textAlign: TextAlign.center,
-                                                ),
-                                              ),
-                                            ),
-                                          ]),
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                );
-                              }).toList(),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 10),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(10, 10, 10, 5),
-                              child: Text(
-                                '${getTitle(current)}',
-                                style: TextStyle(
-                                  fontSize: 24,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                            Text('${getTeam(current)}'),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
+                  onRefresh: () => fetchData(),
+                  child: body(),
                 ),
         );
       },
@@ -281,5 +370,14 @@ class _ViewModel {
       device: store.state.device,
       onSetDeviceName: _onSetDeviceName,
     );
+  }
+}
+
+_launchURL(url) async {
+  // const url = url;
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
